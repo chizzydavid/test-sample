8543d7f1a3edf3645c6f848f54000ea86dd1615f
chizzydavid <davidchizindu@gmail.com>
2024-03-18 22:41:29 +0100

    add health check

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 require('dotenv').config()
 const { App } = require('@slack/bolt')
 const { registerListeners } = require('./listeners');
-
+const nodeCron = require("node-cron");
+const axios = require('axios')
 
 const { SLACK_SIGNING_SECRET, SLACK_BOT_TOKEN, PORT } = process.env
 
 const app = new App({
   token: SLACK_BOT_TOKEN,
-  signingSecret: SLACK_SIGNING_SECRET
+  signingSecret: SLACK_SIGNING_SECRET,
+  customRoutes: [
+    {
+      path: '/health-check',
+      method: ['GET'],
+      handler: (req, res) => {
+        res.writeHead(200);
+        res.end(`Ok!`);
+      },
+    },
+  ]  
 })
 
 registerListeners(app);
 
-app.start(PORT || 9000)
+app.start(PORT || 8000)
   .then(async () => {
-    console.log('Slack application started on 9000')
-  })
-  .catch(e => console.log(e))
+    console.log('Slack application started on 8000')
+  }) 
+  .catch(e => console.log(e, "are you getting here"))
 
